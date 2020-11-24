d9fad8b5600017cd7ab9262ea1a975f79a5e5245
Totoro700 <68616592+Totoro700@users.noreply.github.com>
2020-11-24 15:10:09 -0600

    Update more scripts

--- Program.cs
+++ Program.cs
@@ -16,11 +16,44 @@ namespace C_Sharp
         // Main function (void)
         static void Main(string[] args)
         {
+            // Welcome
             print("Hello!");
+
+            // Ask for name
             print("What\'s your name?");
+
+            // Create and store variable
             string name = Console.ReadLine();
             print("Hello " + name + "!");
-            
+            print("How are you?");
+
+            // Create and store variable
+            string a = Console.ReadLine();
+
+            // Lower chars
+            a = a.ToLower();
+
+            // Check
+            if (a == "good" || a == "good!")
+            {
+                print("Great!");
+            }
+            else if (a == "great" || a == "great!")
+            {
+                print("Awesome!");
+            }
+            else if (a == "bad")
+            {
+                print("");
+            }
+            else if (a == null)
+            {
+                print("Hmm. We couldn't process your answer!");
+            }
+            else
+            {
+                print("Okay then");
+            }
         }
     }
 }
