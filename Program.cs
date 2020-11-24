d7d025d208ed6f831d937a1592e442a62429c8b2
Totoro700 <68616592+Totoro700@users.noreply.github.com>
2020-11-24 15:25:58 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,27 @@ namespace C_Sharp
             Console.WriteLine(msg);
         }
 
+        static bool isYes(string character)
+        {
+            return ("Y" == character || "y" == character);
+        }
+
+        static bool isNo(string character)
+        {
+            return ("N" == character || "n" == character);
+        }
+
+        static void playGame()
+        { 
+            // Play game
+        }
+
+        static void cls()
+        {
+            Console.Clear();
+        }
+
+
         // Main function (void)
         static void Main(string[] args)
         {
@@ -20,7 +41,7 @@ namespace C_Sharp
             print("Hello!");
 
             // Ask for name
-            print("What\'s your name?");
+            print("What's your name?");
 
             // Create and store variable
             string name = Console.ReadLine();
@@ -36,15 +57,47 @@ namespace C_Sharp
             // Check
             if (a == "good" || a == "good!")
             {
+                // Good
                 print("Great!");
             }
             else if (a == "great" || a == "great!")
             {
+                // Great
                 print("Awesome!");
             }
             else if (a == "bad")
             {
-                print("");
+                // Bad
+                // Check if the user wants to play a game
+                print("Do you want to play a game? Y = Yes | N = No ");
+
+                // Create and store variable
+                string b = Console.ReadLine();
+
+                // Check
+                if (isNo(b))
+                {
+                    // No
+                    print("Okay, then");
+
+                    // Do something else
+                }
+                else if (isYes(b))
+                {
+                    // Yes
+                    print("Playing a game . . .");
+                    // Play game
+                    playGame();
+                }
+                else if (b == null)
+                {
+                    // Null
+                    print("We couldn't process process your answer!");
+                }
+                else 
+                {
+                    print("That is not a valid answer!");
+                }
             }
             else if (a == null)
             {
@@ -52,7 +105,7 @@ namespace C_Sharp
             }
             else
             {
-                print("Okay then");
+                print("Hmm. I hope tommorow is a great day!");
             }
         }
     }
