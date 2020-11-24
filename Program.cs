f758b0bfbd076625bddcb57f28cc4f2313ea3570
Totoro700 <68616592+Totoro700@users.noreply.github.com>
2020-11-24 14:51:59 -0600

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿// Use System
+using System;
 
+// Namespace C# "C_Sharp"
 namespace C_Sharp
 {
+    // Class Program "Main"
     class Program
     {
+        // Main function (void)
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
